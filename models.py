2b2d501f224d204f1cd2e6b7c8d755ae00dfd3fa
Sara Gustafsson <sargust@kth.se>
2020-05-13 11:51:35 +0200

    move func to utils.py

--- models.py
+++ models.py
@@ -199,19 +199,3 @@ def step_decay_imagenet(epoch):
     epochs_drop = 10 # NOTE - only difference from cifar10 step decay
     lrate = initial_lrate * math.pow(drop,math.floor((1+epoch)/epochs_drop))
     return lrate
-
-''' ARGS: metrics (metrics object), history (histroy object), dataset_type (str), loss_type (str), noise_type (str), noise_rate (float) '''
-def save_metrics(metrics, history, dataset_type, loss_type, noise_type, noise_rate):
-	curr_test_filename = str(dataset_type) + '_' + str(loss_type) + '_' + str(noise_type) + '_' + str(noise_rate) + '.txt'
-
-	confidence = np.asarray(metrics.confidence)
-	class_accuracies = np.array(metrics.train_acc_class).transpose()
-	prediction_distribution = metrics.Pred[0], metrics.CorrPred[0], metrics.Pred[1], metrics.CorrPred[1]
-	overall_accuracy = history.history['acc']
-	overall_val_accuracy = history.history['val_acc']
-
-	np.savetxt('./Confidence_' + curr_test_filename, confidence)
-	np.savetxt('./AccuracyPerClass_' + curr_test_filename, class_accuracies)
-	np.savetxt('./PredictionDistr_' + curr_test_filename, prediction_distribution)
-	np.savetxt('./Accuracy_' + curr_test_filename, overall_accuracy)
-	np.savetxt('./ValidationAccuracy_' + curr_test_filename, overall_val_accuracy)
